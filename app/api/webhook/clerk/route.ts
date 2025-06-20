977cc75cf6ddd292fc4548defc0a721a270a80c3
Shasha0126 <abhilashakhangar2625@gmail.com>
2025-06-20 13:05:43 +0530

    Successful build: fixed Clerk webhook issues and build errors

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -2,7 +2,7 @@ import { Webhook } from 'svix'
 import { headers } from 'next/headers'
 import { WebhookEvent } from '@clerk/nextjs/server'
 import { createUser, deleteUser, updateUser } from '../../../../lib/actions/user.action'
-import { clerkClient } from '@clerk/nextjs'
+import { clerkClient } from '@clerk/clerk-sdk-node'
 import { NextResponse } from 'next/server'
  
 export async function POST(req: Request) {
@@ -15,7 +15,7 @@ export async function POST(req: Request) {
   }
  
   // Get the headers
-  const headerPayload = headers();
+  const headerPayload = await headers();
   const svix_id = headerPayload.get("svix-id");
   const svix_timestamp = headerPayload.get("svix-timestamp");
   const svix_signature = headerPayload.get("svix-signature");
@@ -57,14 +57,15 @@ export async function POST(req: Request) {
   if(eventType === 'user.created') {
     const { id, email_addresses, image_url, first_name, last_name, username } = evt.data;
 
-    const user = {
-      clerkId: id,
-      email: email_addresses[0].email_address,
-      username: username!,
-      firstName: first_name,
-      lastName: last_name,
-      photo: image_url,
-    }
+  const user = {
+    clerkId: id,
+    email: email_addresses[0].email_address,
+    username: username!,
+    firstName: first_name ?? "",
+    lastName: last_name ?? "",
+    photo: image_url,
+  };
+
 
     const newUser = await createUser(user);
 
@@ -82,12 +83,13 @@ export async function POST(req: Request) {
   if (eventType === 'user.updated') {
     const {id, image_url, first_name, last_name, username } = evt.data
 
-    const user = {
-      firstName: first_name,
-      lastName: last_name,
-      username: username!,
-      photo: image_url,
-    }
+  const user = {
+  firstName: first_name ?? '',   
+  lastName: last_name ?? '',
+  username: username!,
+  photo: image_url,
+};
+
 
     const updatedUser = await updateUser(id, user)
 
